365083576b0a32b0721938b527e53a8960ef68a4
djcjw <soundsationaldjs@comcast.net>
2022-02-06 17:31:12 -0800

    added comment

--- pytech_queries.py
+++ pytech_queries.py
@@ -25,7 +25,7 @@ for doc in student_list:
 
 # find document by student_id
 deb = collection.find_one({"student_id": "1008"})
-
+#testing
 # output the results 
 print("\n  -- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
 print("  Student ID: " + deb["student_id"] + "\n  First Name: " + deb["first_name"] + "\n  Last Name: " + deb["last_name"] + "\n")
