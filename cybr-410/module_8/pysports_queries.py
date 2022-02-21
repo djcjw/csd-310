a4ba710b5070e91cd272df2cf9a2973f28f4b3b4
djcjw <83352035+djcjw@users.noreply.github.com>
2022-02-20 18:01:26 -0800

    Fixed credentials and got code to work via Python

--- cybr-410/module_8/pysports_queries.py
+++ cybr-410/module_8/pysports_queries.py
@@ -5,9 +5,9 @@ from mysql.connector import errorcode
 
 """ database config object """
 config = {
-    "user": "pysports_user",
+    "user": "root",
     "password": "DJChrisW2237!",
-    "localhost": "3306",
+    "host": "localhost",
     "database": "pysports",
     "raise_on_warnings": True
 }
